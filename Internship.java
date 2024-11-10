c0d05eb1336c1e3a3c201edb9acf83663b9e56f7
shaswat2974 <shaswat.singh2974@gmail.com>
2024-11-10 19:50:26 +0530

    Internship.java

---
+++ Internship.java
@@ -0,0 +1,18 @@
+public class Internship {
+    private int id;
+    private String title;
+    private String company;
+    private int duration; // Duration in weeks
+    private String description;
+
+    public Internship(int id, String title, String company, int duration, String description) {
+        this.id = id;
+        this.title = title;
+        this.company = company;
+        this.duration = duration;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    // Similar structure to Job class
+}
