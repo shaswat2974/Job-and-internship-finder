745a55154e98aa8bc0f7203ea85818d8c69a4003
shaswat2974 <shaswat.singh2974@gmail.com>
2024-11-10 19:51:09 +0530

    User.java

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    // Similar structure
+}
